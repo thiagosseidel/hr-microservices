f2ddb6071785f8651779ab4ed2391dbb3c2694dc
Thiago Seidel <thiagosseidel@gmail.com>
2021-06-23 22:30:46 -0300

    Configuring hr-user Project

--- hr-config-server/src/main/java/com/thiagosseidel/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/thiagosseidel/hrconfigserver/HrConfigServerApplication.java
@@ -1,6 +1,5 @@
 package com.thiagosseidel.hrconfigserver;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
