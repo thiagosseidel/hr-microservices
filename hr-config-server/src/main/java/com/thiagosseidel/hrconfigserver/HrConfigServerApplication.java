3f6aecbf721e1f6ba8a615e7d0a4f66c7e3328ee
Thiago Seidel <thiagosseidel@gmail.com>
2021-06-23 21:32:15 -0300

    Private Git Repository

--- hr-config-server/src/main/java/com/thiagosseidel/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/thiagosseidel/hrconfigserver/HrConfigServerApplication.java
@@ -1,15 +1,32 @@
 package com.thiagosseidel.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
+public class HrConfigServerApplication implements CommandLineRunner {
 
+	@Value("${spring.cloud.config.server.git.username}")
+	private String userName;
+	
+	@Value("${spring.cloud.config.server.git.password}")
+	private String passWord;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("USER_NAME = " + userName);
+		
+		System.out.println("PASSWORD = " + passWord);
+		
 	}
 
 }
