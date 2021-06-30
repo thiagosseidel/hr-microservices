229171c6f779b797aefdc73ad35097d4af4af613
Thiago Seidel <thiagosseidel@gmail.com>
2021-06-29 21:20:00 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/thiagosseidel/hroauth/services/UserService.java
@@ -0,0 +1,34 @@
+package com.thiagosseidel.hroauth.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiagosseidel.hroauth.entities.User;
+import com.thiagosseidel.hroauth.feignclients.UserFeignClient;
+
+@Service
+public class UserService {
+
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	public User findByEmail(String email) {
+		
+		User user = userFeignClient.findByEmail(email).getBody();
+		
+		if (user == null) {
+			
+			logger.error("Email not found: " + email);
+			
+			throw new IllegalArgumentException("Email not found");
+		}
+		
+		logger.error("Email found: " + email);
+		
+		return user;
+	}
+}
